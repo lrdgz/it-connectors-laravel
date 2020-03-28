cf4672ef4cf3ffc8308ff0c33e1369278a1a8f4b
Luis Rodriguez <lui.rodriguez@gcnewhorizons.net>
2020-03-28 10:54:23 -0700

    Add Middleware Check Credentials

--- app/Http/Middleware/CheckClientCredentials.php
+++ app/Http/Middleware/CheckClientCredentials.php
@@ -3,18 +3,43 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Laminas\Diactoros\StreamFactory;
+use Laminas\Diactoros\ResponseFactory;
+use Laminas\Diactoros\UploadedFileFactory;
+use Laminas\Diactoros\ServerRequestFactory;
+use Laravel\Passport\Http\Middleware\CheckCredentials;
+use League\OAuth2\Server\Exception\OAuthServerException;
+use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 
-class CheckClientCredentials
+
+class CheckClientCredentials extends CheckCredentials
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param array $scopes
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next,  ...$scopes)
     {
+        $psr = (new PsrHttpFactory(
+            new ServerRequestFactory,
+            new StreamFactory,
+            new UploadedFileFactory,
+            new ResponseFactory
+        ))->createRequest($request);
+
+        try {
+            $psr = $this->server->validateAuthenticatedRequest($psr);
+        } catch (OAuthServerException $e) {
+            return response()->json($e->getPayload(), $e->getHttpStatusCode());
+        }
+
         return $next($request);
     }
+
+    protected function validateCredentials($token) {}
+    protected function validateScopes($token, $scopes) {}
 }
